b9fadf1cafc4f5c3cd2c7548437e2f0662fddab5
ChaitanyaS2104 <chaitanyashankar2104@gmail.com>
2026-06-14 19:34:31 +0530

    First non repeating character

--- first_non_repeating.cpp
+++ first_non_repeating.cpp
@@ -4,31 +4,28 @@
 
 using namespace std;
 
-int first_non_repeating(string str);
+int firstUniqChar(string str);
 
 int main()
 {
     string str;
     getline(cin, str);
-    int res = first_non_repeating(str);
+    int res = firstUniqChar(str);
     cout << res << endl;
     return 0;
 }
 
-int first_non_repeating(string str)
+int firstUniqChar(string s)
 {
-    char non_r;
-    map<char, int> dict;
-    for (int i = 0; i < str.length(); i++)
+    vector<int> freq(26, 0);
+    for (auto &x : s)
     {
-        dict[str[i]]++;
+        freq[x - 'a']++;
     }
-    for (int i = 0; i < str.length(); i++)
+    for (int i = 0; i < s.length(); i++)
     {
-        if (dict[str[i]] == 1)
-        {
+        if (freq[s[i] - 'a'] == 1)
             return i;
-        }
     }
     return -1;
 }
