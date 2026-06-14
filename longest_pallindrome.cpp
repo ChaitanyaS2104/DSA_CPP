9c0a8bd7559bd1f3f899ce7f289d9149968fee38
ChaitanyaS2104 <chaitanyashankar2104@gmail.com>
2026-06-14 21:01:07 +0530

    Longest pallindrome substring

--- longest_pallindrome.cpp
+++ longest_pallindrome.cpp
@@ -1,42 +1,95 @@
-#include<iostream>
-#include<string>
+/*
+We need to find the largest pallindrome in the string and return it with its length
+
+Brute Force
+- Expand around every index (for odd and even pal) and update the largest pallindrome accordingly
+
+Optimal Solution
+
+*/
+ 
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int longest_pallindrome(string str);
+int longest_pallindrome_length(string str);
+string longestPalindrome(string s);
 
-int main(){
+int main()
+{
     string str;
     getline(cin, str);
-    int res = longest_pallindrome(str);
-    cout<<res<<endl;
+    int res = longest_pallindrome_length(str);
+    string res_s = longestPalindrome(str);
+    cout <<res_s<<" Length: "<< res << endl;
     return 0;
 }
 
-int longest_pallindrome(string str){
+int longest_pallindrome_length(string str)
+{
     int max_length = 1;
     int left;
     int right;
-    for(int i=0; i<str.length()-1; i++){
-        int left = i-1;
-        int right = i+1;
+    for (int i = 0; i < str.length() - 1; i++)
+    {
+        int left = i - 1;
+        int right = i + 1;
         // Odd pallindrome
-        while(left>=0 && right<str.length() && str[left] == str[right]){
-            max_length = max(max_length, right-left+1);
+        while (left >= 0 && right < str.length() && str[left] == str[right])
+        {
+            max_length = max(max_length, right - left + 1);
             left--;
             right++;
         }
         left = i;
-        right = i+1;
+        right = i + 1;
         // Even pallindrome
-        while(left>=0 && right<str.length() && str[left] == str[right]){
-            max_length = max(max_length, right-left+1);
+        while (left >= 0 && right < str.length() && str[left] == str[right])
+        {
+            max_length = max(max_length, right - left + 1);
             left--;
             right++;
         }
     }
 
     return max_length;
+}
+
+string longestPalindrome(string s)
+{
+    int w_len = 0;
+    int start = 0;
+
+    for (int i = 0; i < s.length(); i++)
+    {
+        int l = i;
+        int r = i;
+        while (l >= 0 && r < s.length() && s[l] == s[r])
+        {
+            l--;
+            r++;
+        }
+        if (r - l - 1 > w_len)
+        {
+            start = l + 1;
+            w_len = r - l - 1;
+        }
+
+        l = i;
+        r = i + 1;
+        while (l >= 0 && r < s.length() && s[l] == s[r])
+        {
+            l--;
+            r++;
+        }
+        if (r - l - 1 > w_len)
+        {
+            start = l + 1;
+            w_len = r - l - 1;
+        }
+    }
+    return s.substr(start, w_len);
 }
 
 /*
@@ -49,7 +102,7 @@ granddad - max = 1, l=1, r=3, i=2, len=1
   |
 1 >= 0 and 3 < 8 and r != n
 
-granddad 
+granddad
    |
 
 granddad
